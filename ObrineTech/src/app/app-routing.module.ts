2656a74355b0564b2bf0633dd90a636811828c2e
Unknown <jazzy.d1302@outlook.com>
2019-10-07 18:20:06 -0400

    Pc page and header

--- ObrineTech/src/app/app-routing.module.ts
+++ ObrineTech/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PcComponent } from './pc/pc.component';
+import { PhotoComponent } from './photo/photo.component';
+import { DesignComponent } from './design/design.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
 
+
+
+const routes: Routes = [
+  {path: '',component: HomeComponent},
+  {path: 'About',component: AboutComponent},
+  {path: 'Pc_Builds',component: PcComponent},
+  {path: 'Photography',component: PhotoComponent},
+  {path: 'Graphic_Design',component: DesignComponent},
+  {path: 'Portfolio',component: PortfolioComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- ObrineTech/src/app/app.module.ts
+++ ObrineTech/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PcComponent } from './pc/pc.component';
+import { PhotoComponent } from './photo/photo.component';
+import { DesignComponent } from './design/design.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    PcComponent,
+    PhotoComponent,
+    DesignComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
